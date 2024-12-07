b85e22240e6132572b91857077f89e7d23dc29ea
Avik Pal <avikpal@mit.edu>
2024-12-07 20:41:06 +0530

    feat: conditional VAE testcase

--- docs/make.jl
+++ docs/make.jl
@@ -29,7 +29,8 @@ pages = [
             "tutorials/intermediate/1_NeuralODE.md",
             "tutorials/intermediate/2_BayesianNN.md",
             "tutorials/intermediate/3_HyperNet.md",
-            "tutorials/intermediate/4_PINN2DPDE.md"
+            "tutorials/intermediate/4_PINN2DPDE.md",
+            "tutorials/intermediate/5_ConditionalVAE.md",
         ],
         "Advanced" => [
             "tutorials/advanced/1_GravitationalWaveForm.md"
